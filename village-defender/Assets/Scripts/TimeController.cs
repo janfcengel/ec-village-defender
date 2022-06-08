353a13436b57932a0da9fb3c28a4f16c9db6542c
Jan Engel <jen@abatgroup.de>
2022-06-08 10:25:17 +0200

    added time system

---
+++ village-defender/Assets/Scripts/TimeController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class TimeController : MonoBehaviour
+{
+    [SerializeField]
+    private float timeMultiplier;
+    [SerializeField]
+    private float startHour;
+    [SerializeField]
+    private Text timeText;
+
+    private DateTime currentTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = DateTime.Now.Date + TimeSpan.FromHours(startHour);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTimeOfDay();
+    }
+
+    private void UpdateTimeOfDay()
+    {
+        currentTime = currentTime.AddSeconds(Time.deltaTime * timeMultiplier);
+
+        if (timeText != null)
+        {
+            timeText.text = currentTime.ToString("HH:mm");
+        }
+    }
+}
